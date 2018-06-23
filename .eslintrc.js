a6a7ec8ab5c542c2a6539a272b849d9c324fe3b2
Luciano Graziani <lgraziani2712@gmail.com>
2018-06-23 12:40:21 -0300

    Hello world

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@dimax-ar/dimax'],
+  plugins: ['prettier'],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 8,
+    sourceType: 'module',
+  },
+};
