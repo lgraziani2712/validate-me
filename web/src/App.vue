559e2d1792c70a31ffdcfd416122e0c68c7da24f
Luciano Graziani <lgraziani2712@gmail.com>
2018-08-18 21:08:25 -0300

    ⚙️ Improve web with vue example

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
-    <input v-validate-me name="name" required>
-    <span v-show="inputHasError('name')">
-      {{$validateme.firstErrorOf('name')}}
-    </span>
+    <h2>Form</h2>
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <h3>Name</h3>
+        <div>
+          <input v-validate-me="['len:2:10']" name="name" autofocus required>
+          <span v-show="inputHasErrorOrWarning('name')">
+            {{$validateme.firstMessageOf('name')}}
+          </span>
+        </div>
+      </div>
+      <div>
+        <h3>Surname</h3>
+        <div>
+          <input v-validate-me name="surname" required>
+          <span v-show="inputHasErrorOrWarning('surname')">
+            {{$validateme.firstMessageOf('surname')}}
+          </span>
+        </div>
+      </div>
+      <br>
+      <button>Submit form</button>
+    </form>
   </div>
 </template>
 
@@ -12,5 +31,19 @@ import ValidatemeMixin from '@validate-me/vue-plugin/mixin';
 
 export default {
   mixins: [ValidatemeMixin],
+  methods: {
+    handleSubmit() {
+      if (!this.$validateme.validate()) {
+        return;
+      }
+      this.$validateme.beforeSendToServer();
+
+      console.log(this.$validateme.data());
+
+      this.$validateme.process({
+        name: ['unexistingRule'],
+      });
+    },
+  },
 };
 </script>
--- web/src/vue-index.js
+++ web/src/vue-index.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import ValidatemePlugin from '@validate-me/vue-plugin';
+import ValidatemeDictionary from '@validate-me/core/ValidatemeDictionary';
+import ValidatemeRules from '@validate-me/core/ValidatemeRules';
 
 import App from './App';
+
+ValidatemeDictionary.setConfig({
+  clientDictionaryHandler: (lang, name) =>
+    import(`./dictionary/${lang}/${name}`),
+});
+ValidatemeRules.setConfig({
+  clientRulesHandler: name => import(`./rules/${name}`),
+});
 
 Vue.use(ValidatemePlugin);
 
