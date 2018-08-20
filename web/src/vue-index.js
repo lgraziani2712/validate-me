88c145f54afe05d139d83e2be88cd68728d101d5
Luciano Graziani <lgraziani2712@gmail.com>
2018-08-19 21:09:21 -0300

    🔥 Remove custom rules from web

--- web/src/vue-index.js
+++ web/src/vue-index.js
@@ -1,17 +1,7 @@
 import Vue from 'vue';
 import ValidatemePlugin from '@validate-me/vue-plugin';
-import ValidatemeDictionary from '@validate-me/core/ValidatemeDictionary';
-import ValidatemeRules from '@validate-me/core/ValidatemeRules';
 
 import App from './App';
-
-ValidatemeDictionary.setConfig({
-  clientDictionaryHandler: (lang, name) =>
-    import(`./dictionary/${lang}/${name}`),
-});
-ValidatemeRules.setConfig({
-  clientRulesHandler: name => import(`./rules/${name}`),
-});
 
 Vue.use(ValidatemePlugin);
 
